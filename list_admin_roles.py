c6737be1695f6b96f90727cc6821687f739e8ca1
T-Srikanth <51802305+T-Srikanth@users.noreply.github.com>
2020-07-07 03:25:09 +0530

    Update list_admin_roles.py

--- list_admin_roles.py
+++ list_admin_roles.py
@@ -4,9 +4,8 @@ import xlrd, xlwt
 from xlutils.copy import copy as xl_copy
 
 tenantName=""
-#tenantName=enter name with which the output file should be saved
 roleNames={"Identity+Domain+Administrator":"Identity Domain Administrator","Security+Administrator":"Security Administrator","Application+Administrator":"Application Administrator","Audit+Administrator":"Audit Administrator","User+Administrator":"User Administrator"}
-
+# roleName="Identity+Domain+Administrator"
 
 IDCS_SERVICE_URL = ""
 #IDCS_SERVICE_URL=https://<tenant_idcs_id>.identity.oraclecloud.com
@@ -34,9 +33,8 @@ def create_access_tokens():
 
 access_token = create_access_tokens()
 
-def get_userIds_with_role(roleName):
-        list_ids = list()
-        approle_url = IDCS_SERVICE_URL+"/admin/v1/AppRoles?filter=displayName+eq+%22"+roleName+"%22&count=1000"
+def get_approle_id(roleName):
+        approle_url = IDCS_SERVICE_URL+"/admin/v1/AppRoles?filter=displayName+eq+%22"+roleName+"%22"
         approle_headers = {
                 "Authorization": "Bearer "+access_token,
             "Content-Type" : "application/scim+json"
@@ -47,55 +45,39 @@ def get_userIds_with_role(roleName):
         res = requests.get(approle_url, headers = approle_headers,params = params)
         res = json.loads(res._content.decode('utf-8'))
         for i in res["Resources"]:
-            # print(i)
-            for k,v in i.items():
-                if k=="members":
-                    for j in v:
-                        if j["type"]=="User":
-                            list_ids.append(j["value"])
-                    # print(v)
-        with open("id_admin.json","w") as outfile:
-            json_string=json.dumps(res, default=lambda o: o.__dict__, sort_keys=True, indent=2)
-            outfile.write(json_string)
+            ID=i["id"]
 
-        return list_ids
+        return ID
 
-def get_user(id):
-
-        users_url = IDCS_SERVICE_URL+"/admin/v1/Users/"+id
-        users_headers = {
+def get_allusers_with_approle(roleId):
+        dict_users = dict()
+        approle_url = IDCS_SERVICE_URL+"/admin/v1/Users?filter=urn:ietf:params:scim:schemas:oracle:idcs:extension:user:User:approles.value+eq+%22"+roleId+"%22&count=1000"
+        approle_headers = {
                 "Authorization": "Bearer "+access_token,
             "Content-Type" : "application/scim+json"
                 }
         params = {
                 "attributeSets":"all"
                 }
-        res = requests.get(users_url, headers = users_headers,params = params)
+        res = requests.get(approle_url, headers = approle_headers,params = params)
         res = json.loads(res._content.decode('utf-8'))
-        return res["userName"]
-
-def get_userNames_with_role(roleName):
-    list_ids=get_userIds_with_role(roleName)
-    names_list={}
-    for i in list_ids:
-        names_list[i]=(get_user(i))
-    # print(list_names)
-    return names_list
+        for i in res["Resources"]:
+            dict_users[i["displayName"]]=i["id"]
+  
+        return dict_users
 
 def write_details_to_excel(names_list,filename,roleName):
     rb = xlrd.open_workbook(filename, formatting_info=True)
     wb = xl_copy(rb)
-    # test_book=xlwt.Workbook(encoding="utf-8")
     sheet1=wb.add_sheet(roleName)
     row=1
-    sheet1.write(0,0,"userId")
-    sheet1.write(0,1,"userName")
+    sheet1.write(0,0,"displayName")
+    sheet1.write(0,1,"userId")
     for k,v in names_list.items(): 
-        # if row < len(diction)+1:
         sheet1.write(row,0,k)
         sheet1.write(row,1,v)
         row += 1
-    wb.save(filename)
+    wb.save(filename)        
 
 def output_in_excel(filename=tenantName+".xls"):
     book=xlwt.Workbook(encoding="utf-8")
@@ -107,7 +89,9 @@ def output_in_excel(filename=tenantName+".xls"):
     sheet.write(4,0,"User Administrator")
     book.save(filename)
     for k,v in roleNames.items():
-        names_list=get_userNames_with_role(k)
-        write_details_to_excel(names_list,filename,roleName=v)
+        role_id=get_approle_id(roleName=k)
+        names_list=get_allusers_with_approle(roleId=role_id)
+        write_details_to_excel(names_list=names_list,filename=filename,roleName=v)
 
-output_in_excel()        
+output_in_excel()
+
